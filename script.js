3843c7ca33ffc557f7a3eaf9ff7b52cc8aaf4abf
Milywayz <jtjenks@gmail.com>
2023-04-04 10:02:06 -0500

    added a few comments into javascript

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 
+// Arrays used in password options
 let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'h', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 let numbers = [0,1,2,3,4,5,6,7,8,9];
 let special = ["~" , "`" , "!" , "@" , "#" , "$" , "%" , "^" , "&" , "*" , "(" , ")" , "-" , "_" , "+" , "=" , "{" , "}" , "[" , "]" , "|" , "/" , ":" , ";" , "'" , "<" , ">" ,"," , "." , "?"];
 
 
-
+// Function that generates the password
 function generatePassword(){
+  // First prompt of the password and variables that put into 
   let length=prompt("How long do you want your password to be?")
   let passwordoptions=[]
   let finalPassword=[]
