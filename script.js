36c6ce4a09f2fe01910b6efcd40430998b3131e2
Milywayz <jtjenks@gmail.com>
2023-04-04 09:14:23 -0500

    Transfered progress over from clapprepo to its own repo in my github

---
+++ script.js
@@ -0,0 +1,70 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+
+let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'h', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let numbers = [0,1,2,3,4,5,6,7,8,9];
+let special = ["~" , "`" , "!" , "@" , "#" , "$" , "%" , "^" , "&" , "*" , "(" , ")" , "-" , "_" , "+" , "=" , "{" , "}" , "[" , "]" , "|" , "/" , ":" , ";" , "'" , "<" , ">" ,"," , "." , "?"];
+
+
+
+function generatePassword(){
+  let length=prompt("How long do you want your password to be?")
+  let passwordoptions=[]
+  let finalPassword=[]
+  if (length < 8 || length > 128) {
+    alert("Please choose an appropriate length for your password!")
+    return
+  }
+
+  let upper=confirm("Do you want to include Uppercase letters?");
+  let number=confirm("Do you want to include numbers?");
+  let lower=confirm("Do you want to include Lowercase letters?");
+  let specials=confirm("Do you want to include special characters?")
+
+  if (upper === true) {
+    passwordoptions=passwordoptions.concat(upperAlphabet)
+  }
+  if (number === true) {
+    passwordoptions=passwordoptions.concat(numbers)
+  }
+  if (lower === true) {
+    passwordoptions=passwordoptions.concat(lowerAlphabet)
+  }
+  if (specials === true) {
+    passwordoptions=passwordoptions.concat(special)
+  }
+  console.log (passwordoptions)
+
+  // if(confirm("Do you want your password to contain lowercase letters?"))
+  // {
+
+
+  // }
+
+
+
+  for (let i = 0; i < length; i++) {
+    finalPassword.push(passwordoptions[Math.floor(Math.random()*passwordoptions.length)]) 
+
+  }
+
+console.log (finalPassword)
+//build a string based off of randomly selected values from an array
+
+
+  return finalPassword.join("")
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
